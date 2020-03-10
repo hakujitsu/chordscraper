be8468e5d3b5d4212cd1a15c615d1c5b1597a266
hakujitsu <minyihkwek@gmail.com>
2020-03-10 09:34:54 +0800

    Added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chord Scraper
+Python script that extracts chord sheets from https://ja.chordwiki.org/
+Extracted chordsheets are in ChordPro format
--- script.py
+++ script.py
@@ -1,29 +1,37 @@
 import requests
 from bs4 import BeautifulSoup as bs4
 
-# Sharuru
+# シャルル
 # url = 'https://ja.chordwiki.org/wiki/%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%AB'
 
-# Tsukiaitai
-url = 'https://ja.chordwiki.org/wiki/%E8%AA%B0%E3%81%A7%E3%82%82%E3%81%84%E3%81%84%E3%81%8B%E3%82%89%E4%BB%98%E3%81%8D%E5%90%88%E3%81%84%E3%81%9F%E3%81%84'
+# 誰でもいいから付き合いたい
+# url = 'https://ja.chordwiki.org/wiki/%E8%AA%B0%E3%81%A7%E3%82%82%E3%81%84%E3%81%84%E3%81%8B%E3%82%89%E4%BB%98%E3%81%8D%E5%90%88%E3%81%84%E3%81%9F%E3%81%84'
 
-# Hitchcock
+# ヒッチコック
 # url = 'https://ja.chordwiki.org/wiki/%E3%83%92%E3%83%83%E3%83%81%E3%82%B3%E3%83%83%E3%82%AF'
+
+# 高音厨音域テスト
+# url = 'https://ja.chordwiki.org/wiki/%E9%AB%98%E9%9F%B3%E5%8E%A8%E9%9F%B3%E5%9F%9F%E3%83%86%E3%82%B9%E3%83%88'
+
+# Flyers
+url = 'https://ja.chordwiki.org/wiki/Flyers'
 
 
 r = requests.get(url)
 soup = bs4(r.content, 'html.parser')
 
+# Strings to write to chordfile
+chordbody = []
+
 # Title
 title = soup.select_one('.title').contents[1]
-
+chordbody.append("{title: " + title + "}" + "\n")
 
 # Chords
 # main = soup.select('.main')
 
 body = soup.select('p[class="line"]')
 
-chordbody = []
 
 for line in body:
     l = line.contents[1:(len(line) - 1)]
@@ -47,12 +55,13 @@ for line in body:
 
 
 # Writing to file
-outF = open(title + ".txt", "w") 
-for line in chordbody: 
-    outF.write(line) 
-    outF.write("\n") 
 
-outF.close()
+chordFile = open(title + ".cho", "w") 
+for line in chordbody: 
+    chordFile.write(line) 
+    chordFile.write("\n") 
+
+chordFile.close()
 
 
 # Save into file
